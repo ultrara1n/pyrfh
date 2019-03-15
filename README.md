6b9693bc36780e7daf9f8ccf09a8c90c82267c92
Philipp Wensauer <mail@philippwensauer.com>
2019-03-15 14:02:27 +0100

    weniger abhängigkeiten

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pyrfh
 Klausurnoten der RFH Köln lesen und bei Änderung oder Neueintrag Benachrichtigung senden
+
+pip install python-pushover
+pip install requests-html
+pip install requests
--- rfhnoten.py
+++ rfhnoten.py
@@ -1,6 +1,5 @@
 import credentials
 from pushover import init, Client
-import requests
 from requests_html import HTMLSession
 import sqlite3
 import os
@@ -14,20 +13,18 @@ if not os.path.isfile('noten.db'):
 else:
 	conn = sqlite3.connect('noten.db')
 	cursor = conn.cursor()
-	
+
 #Session besorgen
 base_url = "https://www.studse.rfh-koeln.de/?func=login_check"
 payload = {'Benutzer': credentials.rfh_matrikelnr, 'pwd1': credentials.rfh_password}
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
-r = requests.post(base_url, data=payload, headers=headers)
-#print(r.status_code, r.reason)
+session = HTMLSession()
+r = session.post(base_url, data=payload, headers=headers)
 session_id = r.url.split('PHPSESSID=')[1]
-#print(session_id)
 
 #Notenliste besorgen
 noten_url = 'https://www.studse.rfh-koeln.de/vpruef/pruefungsergebnisse.php?PHPSESSID=' + session_id
-session = HTMLSession()
-r = session.get(noten_url)
+r = session.get(noten_url, headers=headers)
 
 table = r.html.find('table', first=True)
 
@@ -39,7 +36,7 @@ for zeile in zeilen:
 	#Leerzeilen ignorieren
 	if content == '':
 		continue
-		
+
 	#Herausfinden was für eine Zeile
 	infos = zeile.find('td', first=True)
 	css_class = infos.attrs['class']
@@ -60,28 +57,28 @@ for zeile in zeilen:
 				note = zelle.text
 			int = int + 1
 
+		#Note ausgeben
+		print(semester + ' - ' + termin + ' - ' + fach + ' - ' + note)
+
 		#Prüfen ob Eintrag in Datenbank, wenn nicht, dann Eintragen
 		cursor.execute('SELECT COUNT(*) FROM noten WHERE semester = "'+semester+'" AND termin = "'+termin+'" AND fach = "'+fach+'" AND note = "'+note+'"')
 		result=cursor.fetchone()
-		
+
 		if result[0] == 0:
 			cursor.execute('INSERT INTO noten (semester, termin, fach, note) VALUES ("'+semester+'","'+termin+'","'+fach+'","'+note+'")')
 			conn.commit()
 			continue
-		
+
 		#Prüfen ob vielleicht schon drin, aber mit anderer Note
 		cursor.execute('SELECT note FROM noten WHERE semester = "'+semester+'" AND termin = "'+termin+'" AND fach = "'+fach+'"')
 		result=cursor.fetchone()
-		
+
 		if result[0] != note:
 			#In Datenbank ändern
 			cursor.execute('UPDATE noten SET note = "'+note+'" WHERE semester = "'+semester+'" AND termin = "'+termin+'" AND fach = "'+fach+'"')
 			conn.commit()
-			
+
 			#Benachrichtigung senden
 			init(credentials.pushover_token)
 			Client(credentials.pushover_secret).send_message(fach+' - '+note, title="RFH Noten")
 			print('Nachfolgende Note geändert:')
-		
-		
-		print(semester + ' - ' + termin + ' - ' + fach + ' - ' + note)
